b2a2c4f5a8d73e0185a1f96092a7f8aaf401fd8d
Gergo06-py <borbelygergo06@gmail.com>
2024-01-18 02:33:10 +0100

    Added more options

--- server.js
+++ server.js
@@ -42,12 +42,22 @@ app.post("/new-member", express.json(), async (req, res) => {
 	res.status(201).json(newFamilyMember);
 });
 
-app.put("/update-member", express.json(), async (req, res) => {
+app.put("/update-member/:id", express.json(), async (req, res) => {
 	const db = client.db("familytree");
-	const updatedFamilyMember = req.body; // Assuming you send JSON body with relevant family member details
-	await db.collection("familymember").updateOne({ _id: updatedFamilyMember._id }, { $set: updatedFamilyMember });
-	res.json(updatedFamilyMember);
-})
+	const id = req.params.id;
+	const updatedMember = req.body;
+	await db
+		.collection("familymember")
+		.updateOne({ id: id }, { $set: updatedMember });
+	res.sendStatus(204);
+});
+
+app.delete("/delete-member/:id", async (req, res) => {
+	const db = client.db("familytree");
+	const id = req.params.id;
+	await db.collection("familymember").deleteOne({ id: id });
+	res.sendStatus(204);
+});
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
